85b13d8a5d538463076c833ff6c2afbe6a380745
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-04-05 13:30:11 -0400

    testing webhook

--- conf.py
+++ conf.py
@@ -1,36 +1,14 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
 import f5_sphinx_theme
 
-
-# -- Project information -----------------------------------------------------
-
 project = 'Docs'
 copyright = '2022, F5'
 author = 'Collaborators'
-
-# The full version, including alpha/beta/rc tags
 release = '202202152222'
 
-# -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
     "sphinxcontrib.youtube",
     "sphinx_copybutton",
@@ -38,7 +16,6 @@ extensions = [
     "sphinx-favicon"
 ]
 
-# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
 # List of patterns, relative to source directory, that match files and
@@ -47,11 +24,6 @@ templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
 #html_theme = ''
 
 #html_sidebars = {'**': ['searchbox.html', 'localtoc.html', 'globaltoc.html']}
